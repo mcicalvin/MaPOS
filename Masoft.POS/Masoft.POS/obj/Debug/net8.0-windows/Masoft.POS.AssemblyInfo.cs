17d8e6113ee23555e16dc910c02e47f19a0103a8
lemo92 <molemoeng92@gmail.com>
2024-09-19 21:47:17 +0200

    Main updates

--- Masoft.POS/Masoft.POS/obj/Debug/net8.0-windows/Masoft.POS.AssemblyInfo.cs
+++ Masoft.POS/Masoft.POS/obj/Debug/net8.0-windows/Masoft.POS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Masoft.POS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d178b607e1e524b56ac8f86620508e36a6f89387")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dabe183cabd35eb24d2a60c1f59fcceaba2191e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Masoft.POS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Masoft.POS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
