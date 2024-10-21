f7ba8159064b9b997dc539a2473e0d4b9490ae4d
Calvin Masindi <mcicalvin@gmail.com>
2024-10-21 18:36:54 +0200

    Added git ignore

--- Masoft.POS/Masoft.POS/obj/Debug/net8.0-windows/Views/Popups/HoldCheckOutPopup.g.i.cs
+++ Masoft.POS/Masoft.POS/obj/Debug/net8.0-windows/Views/Popups/HoldCheckOutPopup.g.i.cs
@@ -55,13 +55,13 @@ namespace Masoft.POS.Views.Popups {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Masoft.POS;V1.0.0.0;component/views/popups/holdcheckoutpopup.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Masoft.POS;component/views/popups/holdcheckoutpopup.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\Popups\HoldCheckOutPopup.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -71,7 +71,7 @@ namespace Masoft.POS.Views.Popups {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
--- Masoft.POS/Masoft.POS/obj/Debug/net8.0-windows/Views/UserControls/UCReportsList.g.i.cs
+++ Masoft.POS/Masoft.POS/obj/Debug/net8.0-windows/Views/UserControls/UCReportsList.g.i.cs
@@ -55,13 +55,13 @@ namespace Masoft.POS.Views.UserControls {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Masoft.POS;V1.0.0.0;component/views/usercontrols/ucreportslist.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Masoft.POS;component/views/usercontrols/ucreportslist.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\UserControls\UCReportsList.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -71,7 +71,7 @@ namespace Masoft.POS.Views.UserControls {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
