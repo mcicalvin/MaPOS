53ac94291206b04866396e4e3bb81a788121b6e4
lemo92 <molemoeng92@gmail.com>
2024-10-22 17:27:22 +0200

    POS PAyment

--- Masoft.POS/Masoft.POS/obj/Debug/net8.0-windows/Views/Popups/SplitBillPopup.g.i.cs
+++ Masoft.POS/Masoft.POS/obj/Debug/net8.0-windows/Views/Popups/SplitBillPopup.g.i.cs
@@ -61,7 +61,7 @@ namespace Masoft.POS.Views.Popups {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Masoft.POS;component/views/popups/splitbillpopup.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Masoft.POS;V1.0.0.0;component/views/popups/splitbillpopup.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Views\Popups\SplitBillPopup.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
